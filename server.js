7d49c1c702472648bfb4ff489617fab1c3df93a2
Omereshone Kelvin Oghenerhoro <kelvinomereshone@gmail.com>
2018-12-14 15:36:44 +0100

    added endpoints for the exercise microservice

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://dominus:trajectory1@ds119800.mlab.com:19800/fcc-backend-js')
+var Schema = mongoose.Schema;
 
 app.use(cors())
 
@@ -17,12 +18,6 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
@@ -42,6 +37,93 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+
+//Setting up models
+//Model to store only usernames
+var User = mongoose.model('User', new Schema({username:{type: String, required: true}}));
+
+//Model to log exercises for corresponding usernames
+var exerciseSchema = new Schema({
+  userId: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: Date
+});
+
+var Exercise = mongoose.model('Exercise', exerciseSchema);
+
+//Setting up post actions
+//To add a new user
+app.post('/api/exercise/new-user', function(req,res) {
+  var username = req.body.username;
+  User.count({username: username}, (err, count) => {
+    if (err) {
+      console.log(err);
+      res.json({"error": err});
+    }
+    if (count > 0) {
+      res.end("<p>User '" + username + "' already exists!!</p>");
+    }
+    const user = new User({username: username});
+    user.save((err,data) => err ? console.log(err) : res.json({"id": data._id, "username": data.username}));
+  })
+});
+
+//To log exercise/activity of exisiting user
+app.post('/api/exercise/add', function(req,res) {
+  var userId = req.body.userId;
+  User.count({user:userId}, (err,count) => {
+    if (err) {
+      console.log(err);
+      res.json({"error": err});
+    }
+    if (count <= 0) {
+      res.end("<p>User '" + userId + "' doesn't exist!!</p>");
+    }
+    const exercise = new Exercise(
+      {
+        userId: req.body.userId,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: new Date(req.body.date)
+      }
+    );
+    exercise.save((err,data) => err ? console.log(err) : res.json({"user":data.userId,"description":data.description,"duration":data.duration,"date":data.date}));
+  })
+});
+
+//Get action to fetch exercise logs for user
+app.get('/api/exercise/log', function(req,res) {
+  if (!req.query.userId) res.end("<p>User Id needed in query!!</p>");
+  else {
+    var query;
+    let response = {};
+    var userId = req.query.userId;
+    var from = req.query.from;
+    var to = req.query.to;
+    
+    if (from && to) 
+      response = {userId: userId, date: { $gt: new Date(from), $lt: new Date(to)}};
+    else if(from) 
+      response = {userId: userId, date: { $gt: new Date(from)}};
+    else if(to) 
+      response = {userId: userId, date: { $lt: new Date(to)}};
+    else 
+      response = {userId: userId};
+    
+    query = req.query.limit ? Exercise.find(response).limit(Number(req.query.limit)) : Exercise.find(response);
+    //Writing response to client
+    query.exec((err,data) => {
+      let count = 0;
+      data.forEach((user) => {
+        res.write("<p>"+ ++count +".</p><p>UserId: "+ user.userId +"</p><p>Description: "+ user.description +"</p><p>Duration: "+ user.duration +"</p><p>Date: "+ user.date +"</p>", ['Transfer-Encoding', 'chunked']);
+      });
+      res.end();
+    });
+  }
+});
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
